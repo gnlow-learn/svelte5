6dbdbf5f62475b0ea919bface3b2b6bf7f5d37f4
Gnlow <gnlow@pm.me>
2025-08-21 00:02:16 +0900

    use hono

--- serve.ts
+++ serve.ts
@@ -1,16 +1,23 @@
-import {
-    Tserve,
-    handleTsLike,
-} from "https://deno.land/x/tserve@0.1.7/mod.ts"
-
+import { Hono } from "https://esm.sh/hono@4.9.2"
+import { createMiddleware } from "https://esm.sh/hono@4.9.2/factory"
+import { serveStatic } from "https://esm.sh/hono@4.9.2/deno"
 import { compile } from "svelte/compiler"
 
-new Tserve([
-    handleTsLike(
-        "svelte",
-        async path => compile(
-            await Deno.readTextFile(path),
-            { dev: true },
-        ).js.code.replaceAll("'svelte", "'https://esm.sh/svelte@5.38.2"),
-    ),
-]).serve()
+const svelte = createMiddleware(async (c, next) => {
+    await next()
+
+    const compiled = compile(
+        await c.res.clone().text(),
+        { dev: true },
+    ).js.code.replaceAll("'svelte", "'https://esm.sh/svelte@5.38.2")
+
+    c.res = new Response(compiled)
+    c.header("content-type", "application/javascript; charset=utf-8")
+})
+
+const app = new Hono()
+
+app.use("/**/*.svelte", svelte)
+app.use("/**/*", serveStatic({ root: "./" }))
+
+Deno.serve(app.fetch)
